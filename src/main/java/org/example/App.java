be461ad82fa66dd94f593ec1d14aa56780c2e392
prosto-rosto4ek <profikprofik2006@gmail.com>
2025-03-27 08:41:01 +0300

    second

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello ИМЯ");
+    System.out.println(Tester.fact(5));
+  }
+}
---
+++ src/main/java/org/example/Tester.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Tester {
+    public String name;
+    public String surname;
+    public int experienceInYears;
+    public String englishLevel;
+    public double salary;
+
+    public Tester() {
+        this("name", "surname", 0, "none", 0.0);
+    }
+
+    public Tester(String name) {
+        this(name, "surname", 0, "none", 0.0);
+    }
+
+    public Tester(String name, String surname) {
+        this(name, surname, 0, "none", 0.0);
+    }
+
+    public Tester(String name, String surname, int experienceInYears, String englishLevel, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.experienceInYears = experienceInYears;
+        this.englishLevel = englishLevel;
+        this.salary = salary;
+    }
+
+    public void displayInfo() {
+        System.out.println("Имя " + name + " Фамилия " + surname + " Опыт " + experienceInYears + " лет  Зарплата " + salary);
+    }
+
+    public void displayInfo(int years) {
+        System.out.println("Имя " + name + " Фамилия " + surname + " Опыт " + (experienceInYears+years) + " лет  Зарплата " + salary);
+    }
+
+    public void displayInfo(double salary) {
+        System.out.println("Имя " + name + " Фамилия " + surname + " Опыт " + experienceInYears + " лет  Зарплата " + (this.salary + salary));
+    }
+
+    public static int fact(int n) {
+        if(n<=1){
+            return 1;
+        }
+        int ret=1;
+        for(int i=2;i<=n;i++){
+            ret*=i;
+        }
+        return ret;
+    }
+}
